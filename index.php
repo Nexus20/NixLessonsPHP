93c405cd65bb1ec503a215abe1c62a72740f827a
Zhenia Ch <chubarov.zheka@gmail.com>
2020-12-12 16:50:28 +0300

    Create start page

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<div class="wrapper">
+    <header class="header">
+        <nav class="header-menu">
+            <ul class="header-list">
+                <li><a href="" class="header-link">Урок 1</a></li>
+                <li><a href="" class="header-link">Урок 2</a></li>
+            </ul>
+        </nav>    
+    </header>
+    <main class="main">
+        <section class="content">
+            <h1>Hello, <b>NIX Education</b></h1>
+        </section>
+    </main>
+    <footer class="footer">
+        <div class="footer-copy">
+            Copy, 2020
+        </div>
+        <div class="footer-text">
+            Lorem ipsum dolor sit amet.
+        </div>
+    </footer>
+</div>
+</body>
+</html>
